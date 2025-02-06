86845037dae415c0f0dfa8fcdbaec3561ed97651
theolucky889 <theolucky889@gmail.com>
2025-02-06 23:12:19 +0800

    index.html dashboard page

--- algorithmic-trading-bot/src/router/index.ts
+++ algorithmic-trading-bot/src/router/index.ts
@@ -1,17 +1,17 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Dashboard from '../Dashboard.vue'
-import Settings from '@/pages/Settings.vue'
-;``
+import { createRouter, createWebHistory } from 'vue-router';
+import Dashboard from './Dashboard.vue';
+import Settings from '@/pages/Settings.vue';
 
 const routes = [
-  { path: '/', component: Dashboard },
-  { path: '/settings', component: Settings },
+  { path: '/', component: Dashboard},
+  { path:'/settings', component: Settings },
   // Add more routes here in the futues
-]
+
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
-})
+});
 
-export default router
+export default router;
