8b728e8b03fe21e5e2cd12b0cd3f63d61a6026d5
theolucky889 <theolucky889@gmail.com>
2025-02-06 22:07:28 +0800

    CHanging react to vue, since vue has better scalability and i am having issues with react and have learned react before and trying other stuff to learn and expand knowledge

--- webapp/backend/app.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from src.execution.broker import execute_trade
-
-app = FastAPI()
-
-@app.post("/execute-trade")
-async def trade(symbol: str, action: str):
-     execute_trade(symbol, action)
-     return {"status": "success"}
